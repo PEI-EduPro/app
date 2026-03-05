9dc74b528ad2c4fc36049dcadb1c13a085a13781
Vasco Pereira <v.pereira@ua.pt>
2026-03-05 11:55:12 +0000

    new exam model

--- api/src/models/exam.py
+++ api/src/models/exam.py
@@ -10,6 +10,10 @@ class Exam(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     exam_config_id: int = Field(foreign_key="exam_config.id")
     exam_xml: Optional[str] = Field(default=None)
+    nmec: Optional[int] = Field(default=None)
+    grade: Optional[float] = Field(default=None)
+    results: Optional[str] = Field(default=None)
+    capture_path: Optional[str] = Field(default=None)
     
     # Relationships
     exam_config: "ExamConfig" = Relationship(back_populates="exams")
@@ -24,17 +28,26 @@ class ExamUpdate(SQLModel):
     """Schema for updating exam data"""
     exam_config_id: Optional[int] = None
     exam_xml: Optional[str] = None
+    nmec: Optional[int] = Field(default=None)
+    grade: Optional[float] = Field(default=None)
+    results: Optional[str] = Field(default=None)
+    capture_path: Optional[str] = Field(default=None)
 
 class ExamRead(SQLModel):
     """Schema for reading exam data"""
     id: int
     exam_config_id: int
     exam_xml: Optional[str] = None
+    nmec: Optional[int] = Field(default=None)
+    grade: Optional[float] = Field(default=None)
+    results: Optional[str] = Field(default=None)
 
 class ExamPublic(SQLModel):
     """Schema for public exam data (no answers exposed)"""
     id: int
     exam_config_id: int
+    grade: Optional[float] = Field(default=None)
+    results: Optional[str] = Field(default=None)
 
     
 
