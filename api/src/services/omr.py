afda34358bb2c1ae72aa89ca1926147ffdfff116
Pedro Martins <00pedromartins@gmail.com>
2026-04-08 14:56:37 +0100

    Added a batch correction endpoint. It validates the received exam's against a waiting room to prevent miscorrections and improper actions from third parties.

--- api/src/services/omr.py
+++ api/src/services/omr.py
@@ -183,7 +183,7 @@ async def evaluate_exam(
 
     # Floor the total score at 0 to prevent negative exam results (optional)
     total_exam_score = max(0.0, total_exam_score)
-    print(f"\n✅ FINAL EXAM SCORE: {total_exam_score:.2f} / 20.00")
+    print(f"\nFINAL EXAM SCORE: {total_exam_score:.2f} / 20.00")
 
     # Set the calculated exam score (can be overwritten by manual validation later on)
     exam.grade = total_exam_score
